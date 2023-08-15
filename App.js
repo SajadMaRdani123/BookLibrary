e1146a4b61e4c5971da9aa9b9a06f12a93d55190
SajadMaRdani123 <sajad123mardani@gmail.com>
2023-08-15 20:12:14 +0430

    initail commit

---
+++ App.js
@@ -0,0 +1,81 @@
+// import { createServer } from "http";
+// import { rename, readFile } from "fs";
+// rename("x.html", "sajad.html", function (err) {
+//   if (err) throw err;
+//   console.log("File Renamed!");
+// });
+// createServer(function (req, res) {
+//   readFile("sajad.html", function (err, data) {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.write(data);
+//     return res.end();
+//   });
+// }).listen(8080);
+///////////////////////////////////////
+// const EventEmitter = require("events");
+
+// const Logger = require("./logger");
+// const logger = new Logger();
+// logger.on("messageLogged", (arg) => {
+//   console.log("listener Called", arg);
+// });
+// logger.log("message");
+////////////////////////////////////////////////
+// const http = require("http");
+// const server = http
+//   .createServer((req, res) => {
+//     if (req.url === "/") {
+//       res.write("hello World");
+//     }
+//   })
+//   .listen(8080);
+
+// console.log("listening on port 3000");
+const express = require("express");
+const app = express();
+app.use(express.json());
+const courses = [
+  {
+    id: 1,
+    name: "course1",
+  },
+  {
+    id: 2,
+    name: "course2",
+  },
+  {
+    id: 3,
+    name: "course3",
+  },
+];
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+app.post("/api/courses", (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    res.status(400).send("Name is required and should be minimum 3 characters");
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+
+  if (!course)
+    res.status(404).send("the course with the given id does not found");
+  res.send(course);
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
